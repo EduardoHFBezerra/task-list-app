99ee274dbe36a246fbd26ecfed933ac4a3f68761
EduardoHFBezerra <eduardohfbezerra@gmail.com>
2022-11-29 18:44:14 -0300

    Adicionando Lista e Campo add atividade

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Area, Header, Container } from './App.styles';
 import { Item } from './types/item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -9,14 +10,40 @@ const App = () => {
     { id: 2, name: 'Comprar um bolo na padaria', done: false }
   ]);
 
+  const handleAddText = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
   return (
     <Container>
       <Area>
         <Header>Lista de Tarefas</Header>
+
+        <AddArea
+          onEnter={handleAddText}
+        />
+
         {list.map((item, index) =>(
           <ListItem 
             key={index}
             item={item}
+            onChange={handleTaskChange}
           />
         ))}
       </Area>
